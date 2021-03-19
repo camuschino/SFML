929990e83d93bfe1bf5700ed36e03584d1b462e8
emmanuel cabral <emmanuelc@bluetrailsoft.com>
2021-03-19 02:24:27 -0300

    improvements algorithms BFS

--- laberth-go/utils/searchingAlgorithms.go
+++ laberth-go/utils/searchingAlgorithms.go
@@ -53,7 +53,6 @@ func checkMapByBFS(player, target models.MapPoint, laberth models.Labyrinth, imd
 
 	for len(slice) > 0 {
 		first := slice[0]
-
 		slice = slice[1:]
 
 		// Check vertical limit in the map.
@@ -86,34 +85,90 @@ func checkMapByBFS(player, target models.MapPoint, laberth models.Labyrinth, imd
 
 		leftPoint = first
 		leftPoint.YPoint--
-
 		if checkLimit(leftPoint.YPoint, fieldDimentionY) && checkMapPoint(leftPoint, laberth) {
 			slice = append(slice, leftPoint)
 		}
 
 		downPoint = first
 		downPoint.XPoint--
-
 		if checkLimit(downPoint.XPoint, fieldDimentionX) && checkMapPoint(downPoint, laberth) {
 			slice = append(slice, downPoint)
 		}
 
 		upPoint = first
 		upPoint.YPoint++
-
 		if checkLimit(upPoint.YPoint, fieldDimentionY) && checkMapPoint(upPoint, laberth) {
 			slice = append(slice, upPoint)
 		}
 
 		rightPoint = first
 		rightPoint.XPoint++
-
 		if checkLimit(rightPoint.XPoint, fieldDimentionX) && checkMapPoint(rightPoint, laberth) {
 			slice = append(slice, rightPoint)
 		}
 	}
 
 	return
+}
+
+func checkMapByDFS(actualPoint, target models.MapPoint, laberth models.Labyrinth, imd *imdraw.IMDraw, win *pixelgl.Window) bool {
+
+	fieldDimentionX := len(laberth.ArrayToMap)
+	fieldDimentionY := len(laberth.ArrayToMap[0])
+
+	// Check vertical limit in the map.
+	if !checkLimit(actualPoint.XPoint, fieldDimentionX) {
+		return false
+	}
+
+	// Check horizontal limit in the map.
+	if !checkLimit(actualPoint.YPoint, fieldDimentionY) {
+		return false
+	}
+
+	// This check if this point is playable, AND
+	// Check if this position is already previously checked.
+	if !checkMapPoint(actualPoint, laberth) {
+		return false
+	}
+
+	laberth.ArrayToCheck[actualPoint.XPoint][actualPoint.YPoint] = true
+
+	if target == actualPoint {
+		renderingStep(actualPoint, laberth.SizeField, colornames.Blue, imd, win)
+		time.Sleep(1000 * time.Millisecond)
+		return true
+	}
+
+	renderingStep(actualPoint, laberth.SizeField, colornames.Greenyellow, imd, win)
+
+	time.Sleep(10 * time.Millisecond)
+
+	leftPoint := actualPoint
+	leftPoint.XPoint--
+	if checkMapByDFS(leftPoint, target, laberth, imd, win) {
+		return true
+	}
+
+	downPoint := actualPoint
+	downPoint.YPoint--
+	if checkMapByDFS(downPoint, target, laberth, imd, win) {
+		return true
+	}
+
+	rightPoint := actualPoint
+	rightPoint.XPoint++
+	if checkMapByDFS(rightPoint, target, laberth, imd, win) {
+		return true
+	}
+
+	upPoint := actualPoint
+	upPoint.YPoint++
+	if checkMapByDFS(upPoint, target, laberth, imd, win) {
+		return true
+	}
+
+	return false
 }
 
 func checkLimit(currentValue, limit int) bool {
@@ -130,73 +185,4 @@ func checkPointIsWall(point models.MapPoint, laberth models.Labyrinth) bool {
 
 func checkPointIsAlreadyTested(point models.MapPoint, laberth models.Labyrinth) bool {
 	return laberth.ArrayToCheck[point.XPoint][point.YPoint]
-}
-
-func checkMapByDFS(actualPoint, target models.MapPoint, laberth models.Labyrinth, imd *imdraw.IMDraw, win *pixelgl.Window) bool {
-
-	fieldDimentionX := len(laberth.ArrayToMap)
-	fieldDimentionY := len(laberth.ArrayToMap[0])
-
-	yActual := actualPoint.YPoint
-	xActual := actualPoint.XPoint
-
-	// Check vertical limit in the map.
-	if !checkLimit(xActual, fieldDimentionX) {
-		return false
-	}
-
-	// Check horizontal limit in the map.
-	if !checkLimit(yActual, fieldDimentionY) {
-		return false
-	}
-
-	// This check if this point is playable.
-	if laberth.ArrayToMap[xActual][yActual] {
-		return false
-	}
-
-	// Check if this position is already previously checked.
-	if laberth.ArrayToCheck[xActual][yActual] {
-		return false
-	}
-
-	laberth.ArrayToCheck[xActual][yActual] = true
-
-	if target == actualPoint {
-		imd.Color = colornames.Red
-		px := getWall(xActual, yActual, laberth.SizeField)
-		imd.Push(px.Min, px.Max)
-		imd.Rectangle(0)
-		imd.Draw(win)
-		win.Update()
-		time.Sleep(1000 * time.Millisecond)
-		return true
-	}
-
-	imd.Color = colornames.Yellow
-	px := getWall(xActual, yActual, laberth.SizeField)
-	imd.Push(px.Min, px.Max)
-	imd.Rectangle(0)
-	imd.Draw(win)
-	win.Update()
-
-	time.Sleep(10 * time.Millisecond)
-
-	if checkMapByDFS(models.MapPoint{XPoint: xActual - 1, YPoint: yActual}, target, laberth, imd, win) {
-		return true
-	}
-
-	if checkMapByDFS(models.MapPoint{XPoint: xActual, YPoint: yActual - 1}, target, laberth, imd, win) {
-		return true
-	}
-
-	if checkMapByDFS(models.MapPoint{XPoint: xActual + 1, YPoint: yActual}, target, laberth, imd, win) {
-		return true
-	}
-
-	if checkMapByDFS(models.MapPoint{XPoint: xActual, YPoint: yActual + 1}, target, laberth, imd, win) {
-		return true
-	}
-
-	return false
 }
